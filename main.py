90acaee762483762af8cdfd679714184cfea1dd9
Mosè <capelli.mose.studente@itispaleocapa.it>
2025-09-15 22:43:02 +0100

    Creazione della password con opzione di maiscole, numeri e caratteri speciali

---
+++ main.py
@@ -0,0 +1,96 @@
+import sys
+import random
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QSlider, QCheckBox
+from PyQt5.QtCore import Qt
+
+class Finestra(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Esempio PyQt5")
+        self.resize(400, 300)
+
+        # Layout verticale
+        layout = QVBoxLayout()
+
+        # Etichetta per mostrare il valore dello slider
+        self.label = QLabel("Lunghezza: 10")
+        layout.addWidget(self.label)
+
+        # Slider (da 5 a 32)
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(5)
+        self.slider.setMaximum(32)
+        self.slider.setValue(10)  # valore iniziale
+        self.slider.valueChanged.connect(self.aggiorna_testo)
+        layout.addWidget(self.slider)
+
+        # Checkbox 1
+        self.check_maiuscole = QCheckBox("Maiuscole (A-Z)")
+        self.check_maiuscole.setChecked(True)  # attiva di default
+        self.check_maiuscole.stateChanged.connect(self.aggiorna_testo)
+        layout.addWidget(self.check_maiuscole)
+
+        # Checkbox 2
+        self.check_numeri = QCheckBox("Numeri (0-9)")
+        self.check_numeri.setChecked(False)
+        self.check_numeri.stateChanged.connect(self.aggiorna_testo)
+        layout.addWidget(self.check_numeri)
+
+
+         # Checkbox 3
+        self.check_carspec = QCheckBox("Caratteri speciali (!, @, #, ...)")
+        self.check_carspec.setChecked(False)
+        self.check_carspec.stateChanged.connect(self.aggiorna_testo)
+        layout.addWidget(self.check_carspec)
+        self.setLayout(layout)
+
+    def aggiorna_testo(self):
+        lunghezza = self.slider.value()
+        caratteri = list("abcdefghijklmnopqrstuvwxyz")
+        speciali = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"]
+        x=0
+        psw=""
+        while x<lunghezza:
+            psw += str(random.choice(caratteri))
+            x+=1
+
+        if self.check_maiuscole.isChecked():
+            num = random.randint(int(lunghezza/4), int(lunghezza/2.5))
+            while num>0:
+                pos = random.randint(0, lunghezza-1)
+                pswlist = list(psw)
+                pswlist[pos] = pswlist[pos].upper()
+                psw = "".join(pswlist)
+                num-=1
+
+        if self.check_numeri.isChecked():
+            num = random.randint(int(lunghezza/5), int(lunghezza/3))
+            while num>0:
+                pos = random.randint(0, lunghezza-1)
+                pswlist = list(psw)
+                pswlist[pos] = str(random.randint(0, 9))
+                psw = "".join(pswlist)
+                num-=1
+        
+        if self.check_carspec.isChecked():
+            num = random.randint(int(lunghezza/5), int(lunghezza/3))
+            while num>0:
+                pos = random.randint(0, lunghezza-1)
+                pswlist = list(psw)
+                pswlist[pos] = random.choice(speciali)
+                psw = "".join(pswlist)
+                num-=1
+
+
+        self.label.setText(psw)
+        
+
+# Avvio applicazione
+app = QApplication(sys.argv)
+finestra = Finestra()
+finestra.show()
+sys.exit(app.exec_())
+
+
+
+
